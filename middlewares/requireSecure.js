49bf089dfc5ed4c2d69e0d2990e135f903a539af
fortheworld2017 <maksimmosko0303@yahoo.com>
2018-03-31 07:17:36 +0800

    'getip'

--- middlewares/requireSecure.js
+++ middlewares/requireSecure.js
@@ -1,5 +1,9 @@
 module.exports = (req, res, next) => {
-  if (!req.user) {
+  var ip = (req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress).split(",")[0];
+  if (ip !== 'server') {
     return res.status(403).send('Forbidden');
   }
   next();
